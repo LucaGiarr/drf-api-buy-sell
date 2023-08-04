d09fe6fd7b1b4bba96a53ad5baacf9cc5883dfd7
LucaGiarr <giarrusso.luca8@gmail.com>
2023-08-04 17:01:35 +0000

    add cars serializers and cars view

---
+++ cars/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from cars import views
+
+urlpatterns = [
+    path('cars/', views.CarList.as_view()),
+    # path('cars/<int:pk>/', views.CarDetail.as_view()),
+]
--- cars/views.py
+++ cars/views.py
@@ -1,1 +1,11 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Car
+from .serializers import CarSerializer
+
+
+class CarList(APIView):
+    def get(self, request):
+        cars = Car.objects.all()
+        serializer = CarSerializer(cars, many=True)
+        return Response(serializer.data)
