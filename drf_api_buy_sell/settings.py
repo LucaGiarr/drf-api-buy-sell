b26146df1fbcbd000fefb2b8e112fe3a1a62e72c
LucaGiarr <giarrusso.luca8@gmail.com>
2023-08-07 18:16:32 +0000

    edit ALLOWED_HOSTS and CLIENT_ORIGIN_DEV

--- drf_api_buy_sell/settings.py
+++ drf_api_buy_sell/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
+import re
 
 if os.path.exists('env.py'):
     import env
@@ -64,8 +65,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True if 'DEV' in os.environ else False
 
-ALLOWED_HOSTS = ['localhost', 'drf-api-buy-sell-f7b7b7adb241.herokuapp.com']
-
+ALLOWED_HOSTS = ['localhost', os.environ.get('ALLOWED_HOST')]
 
 # Application definition
 
@@ -107,13 +107,10 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-if 'CLIENT_ORIGIN' in os.environ:
-    CORS_ALLOWED_ORIGINS = [
-        os.environ.get('CLIENT_ORIGIN')
-    ]
-else:
+if 'CLIENT_ORIGIN_DEV' in os.environ:
+    extracted_url = re.match(r'^.+-', os.environ.get('CLIENT_ORIGIN_DEV', ''), re.IGNORECASE).group(0)
     CORS_ALLOWED_ORIGIN_REGEXES = [
-        r"^https://.*\.gitpod\.io$",
+        rf"{extracted_url}(eu|us)\d+\w\.gitpod\.io$",
     ]
 
 CORS_ALLOW_CREDENTIALS = True
