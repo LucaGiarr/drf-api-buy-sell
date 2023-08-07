75c23ca8ca460d2b5b10bc373d5b42fd063e9f29
LucaGiarr <giarrusso.luca8@gmail.com>
2023-08-07 14:55:20 +0000

    refactor car and profile views to generic views

--- cars/views.py
+++ cars/views.py
@@ -1,70 +1,21 @@
-from django.http import Http404
-from rest_framework import status, permissions
-from rest_framework.views import APIView
-from rest_framework.response import Response
+from rest_framework import generics, permissions
 from .models import Car
 from .serializers import CarSerializer
 from drf_api_buy_sell.permissions import IsOwnerOrReadOnly
 
 
-class CarList(APIView):
+class CarList(generics.ListCreateAPIView):
     serializer_class = CarSerializer
     permission_classes = [
         permissions.IsAuthenticatedOrReadOnly
     ]
+    queryset = Car.objects.all()
 
-    def get(self, request):
-        cars = Car.objects.all()
-        serializer = CarSerializer(
-            cars, many=True, context={'request': request})
-        return Response(serializer.data)
-
-    def post(self, request):
-        serializer = CarSerializer(
-            data=request.data, context={'request': request}
-        )
-        if serializer.is_valid():
-            serializer.save(owner=request.user)
-            return Response(
-                serializer.data, status=status.HTTP_201_CREATED
-            )
-        return Response(
-            serializer.errors, status=status.HTTP_400_BAD_REQUEST
-            )
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
 
 
-class CarDetail(APIView):
+class CarDetail(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = CarSerializer
     permission_classes = [IsOwnerOrReadOnly]
-
-    def get_object(self, pk):
-        try:
-            car = Car.objects.get(pk=pk)
-            self.check_object_permissions(self.request, car)
-            return car
-        except Car.DoesNotExist:
-            raise Http404
-
-    def get(self, request, pk):
-        car = self.get_object(pk)
-        serializer = CarSerializer(
-            car, context={'request': request}
-        )
-        return Response(serializer.data)
-
-    def put(self, request, pk):
-        car = self.get_object(pk)
-        serializer = CarSerializer(
-            car, data=request.data, context={'request': request}
-        )
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, pk):
-        car = self.get_object(pk)
-        car.delete()
-        return Response(
-            status=status.HTTP_204_NO_CONTENT
-        )
+    queryset = Car.objects.all()
