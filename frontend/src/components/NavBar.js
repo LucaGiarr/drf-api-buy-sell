6f1d650eb7c219ad1593d0ca2d0025bcec70e1a2
LucaGiarr <giarrusso.luca8@gmail.com>
2023-08-12 11:00:22 +0000

    add username in the navbar when user is logged in

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import {Navbar, Container, Nav} from "react-bootstrap"
 import styles from "../styles/NavBar.module.css"
 import { NavLink } from 'react-router-dom'
+import { CurrentUserContext } from "../App";
 
 const NavBar = () => {
+
+  const currentUser = useContext(CurrentUserContext);
+
+  const loggedInIcons = <>{currentUser?.username}</>;
+  const loggedOutIcons = (
+    <>
+      <NavLink exact to="/signin" className={styles.NavLink} activeClassName={styles.Active}>Sign In</NavLink>
+      <NavLink exact to="/signup" className={styles.NavLink} activeClassName={styles.Active}>Sign Up</NavLink>
+    </>
+  );
+
   return (
     <Navbar className={styles.NavBar} expand="md" fixed='top'>
         <Container>
@@ -15,8 +27,9 @@ const NavBar = () => {
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="ml-auto text-center">
                     <NavLink exact to="/" className={styles.NavLink} activeClassName={styles.Active}>Home</NavLink>
-                    <NavLink exact to="/signin" className={styles.NavLink} activeClassName={styles.Active}>Sign In</NavLink>
-                    <NavLink exact to="/signup" className={styles.NavLink} activeClassName={styles.Active}>Sign Up</NavLink>
+
+                    {/* to be re-styled */}
+                    <NavLink exact to="/" className={styles.NavLink} activeClassName={styles.Active}>{currentUser ? loggedInIcons : loggedOutIcons}</NavLink>
                 </Nav>
             </Navbar.Collapse>
         </Container>
